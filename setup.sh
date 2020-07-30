5d5c845eb1c030539bbb94d3e177e9d9ce50666a
Jared Engler <jared.lite@gmail.com>
2020-07-30 02:41:18 -0500

    refactor setup.sh for less indents

--- setup.sh
+++ setup.sh
@@ -4,19 +4,21 @@ declare -a filenames=("$@")
 
 if [ ${#filenames[@]} -eq 0 ]; then
     echo "usage: setup.sh [filenames]"
-else
-    for file in "${filenames[@]}"
-    do
-        if [[ ! $file =~ "setup.sh" ]]; then
-            echo "setting up $file"
-            if [ -e ~/.$file ]; then
-                if [ -L ~/.$file ]; then
-                    rm ~/.$file
-                else
-                    mv --backup=numbered ~/.$file ~/.$file.bak
-                fi
-            fi
-            ln -s $(dirname $(readlink -f $0))/$file ~/.$file;
-        fi
-    done
+    exit 1
 fi
+
+for file in "${filenames[@]}"; do
+    if [[ ! $file =~ "setup.sh" ]]; then
+        echo "setting up $file"
+        if [ -e ~/.$file ]; then
+            if [ -L ~/.$file ]; then
+                rm ~/.$file
+            else
+                mv --backup=numbered ~/.$file ~/.$file.bak
+            fi
+        fi
+        ln -s $(dirname $(readlink -f $0))/$file ~/.$file;
+    fi
+done
+
+exit 0
